d18166e8154dca99ddd217a7696a9a7e7eff6480
RuofanBie9729 <84932323+RuofanBie9729@users.noreply.github.com>
2023-04-26 13:30:57 -0400

    ML:KNN+RF

---
+++ analysis.R
@@ -0,0 +1,41 @@
+N <- c(500, 800, 1000, 1500, 2000, 2500)
+DR_res <- NULL
+for(n in N){
+	filename <- paste0("hat_p_misspecified_n", n, ".rds")
+	test <- readRDS(filename)
+	Res <- sapply(test, function(x){x[[1]]})
+      #Res <- na.omit(t(Res))
+	ATE <- apply(Res, 2, mean)
+      DR_bias <- ATE[c(4, 6:8)]-ATE[1]
+	DR_res <- rbind(DR_res, cbind(rep(n,4), DR_bias, c("DR", "DR_rf", "DRSP", "DRSP_rf")))
+}
+
+DR_res <- as.data.frame(DR_res)
+colnames(DR_res) <- c("N", "bias", "estimator")
+library(ggplot2)
+ggplot(DR_res, aes(x=as.numeric(N), y=as.numeric(bias), color=estimator, group=estimator)) +
+	geom_line()+
+	geom_point()+
+	geom_hline(yintercept=0, linetype="dashed", color="red")
+
+test <- readRDS("hat_p_misspecified_n500.rds")
+length(test)
+Res <- sapply(test, function(x){x[[1]]})
+boot_res <- sapply(test, function(x){x[[2]]})
+apply(Res, 1, mean)
+
+boot_res <- rbind(Res[1,], boot_res)
+boot_ci <- function(x){
+  true = x[1]
+  x = x[2:length(x)]
+  est_boot <- function(i){
+    boot <- x[((i-1)*100+1):(i*100)]
+    boot_lower <- quantile(sort(boot), 0.025)
+    boot_upper <- quantile(sort(boot), 0.975)
+    return((boot_lower <= true) && (boot_upper >= true))
+  }
+  coverage <- sapply(1:7, est_boot)
+  return(coverage)
+}
+coverage_res <- apply(boot_res, 2, boot_ci)
+apply(coverage_res, 1, mean)
--- toyexample_n2000.R
+++ toyexample_n2000.R
@@ -1,12 +1,13 @@
+simulation <- function(seed){
 #####one trial and one target without missing data
-set.seed(123456)
+set.seed(seed)
 library(mvtnorm)
-library(randomForest)
+library(ranger)
 source("beta0_solver.R")
 
 p=5 # number of covariates
 rho=0.5 # covariance between covariates
-N=3000 #total sample size
+N=4000 #total sample size
 marg=0.5
 
 ###coefficients for trial participation deciding model
@@ -34,8 +35,6 @@ Y_gen <- function(Y){
   return(random_num<expY)
 }
 
-Res <- NULL
-for(rep in 1:100){
 
 Subjects <- rmvnorm(N, rep(0, 3), Sigma)
 Subjects <- cbind(Subjects, matrix(rbinom(N*2, 1, 0.5), ncol=2))
@@ -73,6 +72,27 @@ colnames(TrialData) <- c("V1", "V2", "V3", "V4", "V5", "trt", "Y")
 Target <- as.data.frame(Target)
 colnames(Target) <- c("V1", "V2", "V3", "V4", "V5", "trt", "Y")
 
+case_weight <- function(data, Y){
+  w <- 1/table(data[,"Y"])
+  w <- w/sum(w)
+  weights <- rep(0, nrow(data))
+  weights[data[,"Y"]==0] = w["0"]
+  weights[data[,"Y"]==1] = w["1"]
+  return(weights)
+}
+
+get_prob <- function(data, Y, k=50){
+  featureMatrix <- data[,1:5]
+  KNNInds <- FNN::get.knn(featureMatrix, k = k)$nn.index
+  est_prob <- sapply(
+    1:nrow(featureMatrix),
+    function(i){
+      nnObs <- data[KNNInds[i, ],]
+      mean(nnObs[,Y])
+    })
+  return(est_prob)
+}
+
 Phi_hat <- function(a, Trials){
   variable_name <- names(Trials)[1:p]
   cpt_variable_name <- variable_name[!is.na(apply(Trials[,1:p], 2, mean))] ### used for complete-variable estimators 
@@ -84,27 +104,34 @@ Phi_hat <- function(a, Trials){
   In_Trial <- c(rep(1, nrow(Trials)), rep(0, nrow(Target)))
   Whole <- cbind(Whole, In_Trial)
   #### compute hat_p
-  hat_p_fit <- glm(as.formula(paste0("Y~", covariates_outcome)), data=Whole, family="binomial")
-  hat_p <- sapply(predict(hat_p_fit, newdata=Whole), expit)
-  
+  hat_p_fit <- glm(as.formula(paste0("In_Trial~", covariates)), data=Whole, family="binomial")
+  hat_p <- predict(hat_p_fit, newdata=Whole, type="response")
+
   #### ML prediction
-  hat_p_fit_rf <- randomForest(as.formula(paste0("Y~", covariates_outcome)), data=Whole, mtry=5)
-  hat_p_rf <- predict(hat_p_fit_rf, newdata=Whole, type="response")
+  In_Trial_rf <- get_prob(Whole, "In_Trial")
+  hat_p_fit_rf <- ranger(In_Trial_rf~V1+V2+V3+V4+V5, data=Whole)
+  #hat_p_fit_rf <- randomForest(as.formula(paste0("In_Trial_rf~", covariates)), data=Whole)
+  hat_p_rf <- predict(hat_p_fit_rf, data=Whole, type="response")$predictions
+  #hat_p_rf[hat_p_rf<=0.05] <- 0.05
 
   ### compute ga
   Trials_arm <- Trials[which(Trials$trt==a),]
   ga_fit <- glm(as.formula(paste0("Y~", covariates_outcome)), data=Trials_arm, family="binomial")
-  ga <- sapply(predict(ga_fit, newdata=Whole), expit)
+  ga <- predict(ga_fit, newdata=Whole, type="response")
 
   ### ML ga prediction
-  ga_fit_rf <- randomForest(as.formula(paste0("Y~", covariates_outcome)), data=Trials_arm, mtry=5)
-  ga_rf <- predict(ga_fit_rf, newdata=Whole, type="response")
+  Y_rf <- get_prob(Trials_arm, "Y")
+  Trials_arm$V22 <- Trials_arm[,"V2"]^2
+  Whole$V22 <- Whole[,"V2"]^2
+  ga_fit_rf <- ranger(Y_rf~V1+V2+V3+V4+V5+V22, data=Trials_arm)
+  ga_rf <- predict(ga_fit_rf, data=Whole, type="response")$predictions
 
   ### compute ea1
+  trt_rf <- get_prob(Trials, "trt")
   ea1_fit <- glm(as.formula(paste0("trt~", covariates)), data=Trials, family="binomial")
-  ea1_fit_rf <- randomForest(as.formula(paste0("trt~", covariates)), data=Trials, mtry=5)
-  ea1 <- sapply(predict(ea1_fit, Whole), expit)  
-  ea1_rf <- predict(ea1_fit_rf, newdata=Whole, type="response")
+  ea1_fit_rf <- ranger(trt_rf~V1+V2+V3+V4+V5, data=Trials)  
+  ea1 <- predict(ea1_fit, Whole, type="response") 
+  ea1_rf <- predict(ea1_fit_rf, data=Whole, type="response")$predictions
   ea0 <- 1-ea1
   ea0_rf <- 1-ea1_rf
 
@@ -165,46 +192,56 @@ DR_split <- function(a, Trials){
   Whole_S1 <- cbind(Whole_S1, In_Trial_S1)
   Whole_S2 <- cbind(Whole_S2, In_Trial_S2)
   #### compute hat_p
-  hat_p_fit_S1 <- glm(as.formula(paste0("Y~", covariates_outcome)), data=Whole_S1, family="binomial")
-  hat_p_S2 <- sapply(predict(hat_p_fit_S1, newdata=Whole_S2), expit)
+  hat_p_fit_S1 <- glm(as.formula(paste0("In_Trial_S1~", covariates)), data=Whole_S1, family="binomial")
+  hat_p_S2 <- predict(hat_p_fit_S1, newdata=Whole_S2, type="response")
 
-  hat_p_fit_S2 <- glm(as.formula(paste0("Y~", covariates_outcome)), data=Whole_S2, family="binomial")
-  hat_p_S1 <- sapply(predict(hat_p_fit_S2, newdata=Whole_S1), expit)
+  hat_p_fit_S2 <- glm(as.formula(paste0("In_Trial_S2~", covariates)), data=Whole_S2, family="binomial")
+  hat_p_S1 <- predict(hat_p_fit_S2, newdata=Whole_S1, type="response")
   
   #### ML prediction
-  hat_p_fit_rf_S1 <- randomForest(as.formula(paste0("Y~", covariates_outcome)), data=Whole_S1, mtry=5)
-  hat_p_rf_S2 <- predict(hat_p_fit_rf_S1, newdata=Whole_S2, type="response")
+  In_Trial_rf_S1 <- get_prob(Whole_S1, "In_Trial_S1")
+  hat_p_fit_rf_S1 <- ranger(In_Trial_rf_S1~V1+V2+V3+V4+V5, data=Whole_S1)
+  hat_p_rf_S2 <- predict(hat_p_fit_rf_S1, data=Whole_S2, type="response")$predictions
 
-  hat_p_fit_rf_S2 <- randomForest(as.formula(paste0("Y~", covariates_outcome)), data=Whole_S2, mtry=5)
-  hat_p_rf_S1 <- predict(hat_p_fit_rf_S2, newdata=Whole_S1, type="response")
+  In_Trial_rf_S2 <- get_prob(Whole_S2, "In_Trial_S2")
+  hat_p_fit_rf_S2 <- ranger(In_Trial_rf_S2~V1+V2+V3+V4+V5, data=Whole_S2)
+  hat_p_rf_S1 <- predict(hat_p_fit_rf_S2, data=Whole_S1, type="response")$predictions
 
   ### compute ga
   Trials_arm_S1 <- Trials_S1[which(Trials_S1$trt==a),]
   Trials_arm_S2 <- Trials_S2[which(Trials_S2$trt==a),]
   ga_fit_S1 <- glm(as.formula(paste0("Y~", covariates_outcome)), data=Trials_arm_S1, family="binomial")
-  ga_S2 <- sapply(predict(ga_fit_S1, newdata=Whole_S2), expit)
+  ga_S2 <- predict(ga_fit_S1, newdata=Whole_S2, type="response")
   ga_fit_S2 <- glm(as.formula(paste0("Y~", covariates_outcome)), data=Trials_arm_S2, family="binomial")
-  ga_S1 <- sapply(predict(ga_fit_S2, newdata=Whole_S1), expit)
+  ga_S1 <- predict(ga_fit_S2, newdata=Whole_S1, type="response")
 
   #### ML ga prediction
-  ga_fit_rf_S1 <- randomForest(as.formula(paste0("Y~", covariates_outcome)), data=Trials_arm_S1, mtry=5)
-  ga_rf_S2 <- predict(ga_fit_rf_S1, newdata=Whole_S2, type="response")
+  Y_rf_S1 <- get_prob(Trials_arm_S1, "Y")
+  Trials_arm_S1$V22 <- Trials_arm_S1[,"V2"]^2
+  Whole_S2$V22 <- Whole_S2[,"V2"]^2 
+  ga_fit_rf_S1 <- ranger(Y_rf_S1~V1+V2+V3+V4+V5+V22, data=Trials_arm_S1)
+  ga_rf_S2 <- predict(ga_fit_rf_S1, data=Whole_S2, type="response")$predictions
 
-  ga_fit_rf_S2 <- randomForest(as.formula(paste0("Y~", covariates_outcome)), data=Trials_arm_S2, mtry=5)
-  ga_rf_S1 <- predict(ga_fit_rf_S2, newdata=Whole_S1, type="response")
+  Y_rf_S2 <- get_prob(Trials_arm_S2, "Y")
+  Trials_arm_S2$V22 <- Trials_arm_S2[,"V2"]^2
+  Whole_S1$V22 <- Whole_S1[,"V2"]^2 
+  ga_fit_rf_S2 <- ranger(Y_rf_S2~V1+V2+V3+V4+V5+V22, data=Trials_arm_S2)
+  ga_rf_S1 <- predict(ga_fit_rf_S2, data=Whole_S1, type="response")$predictions
 
   ### compute ea1
+  trt_rf_S1 <- get_prob(Trials_S1, "trt")
   ea1_fit_S1 <- glm(as.formula(paste0("trt~", covariates)), data=Trials_S1, family="binomial")
-  ea1_fit_rf_S1 <- randomForest(as.formula(paste0("trt~", covariates)), data=Trials_S1, mtry=5)
-  ea1_S2 <- sapply(predict(ea1_fit_S1, Whole_S2), expit)  
-  ea1_rf_S2 <- predict(ea1_fit_rf_S1, newdata=Whole_S2, type="response")
+  ea1_fit_rf_S1 <- ranger(trt_rf_S1~V1+V2+V3+V4+V5, data=Trials_S1)
+  ea1_S2 <- predict(ea1_fit_S1, Whole_S2, type="response") 
+  ea1_rf_S2 <- predict(ea1_fit_rf_S1, data=Whole_S2, type="response")$predictions
   ea0_S2 <- 1-ea1_S2
   ea0_rf_S2 <- 1-ea1_rf_S2
 
+  trt_rf_S2 <- get_prob(Trials_S2, "trt")
   ea1_fit_S2 <- glm(as.formula(paste0("trt~", covariates)), data=Trials_S2, family="binomial")
-  ea1_fit_rf_S2 <- randomForest(as.formula(paste0("trt~", covariates)), data=Trials_S2, mtry=5)
-  ea1_S1 <- sapply(predict(ea1_fit_S2, Whole_S1), expit)  
-  ea1_rf_S1 <- predict(ea1_fit_rf_S2, newdata=Whole_S1, type="response")
+  ea1_fit_rf_S2 <- ranger(trt_rf_S1~V1+V2+V3+V4+V5, data=Trials_S2)
+  ea1_S1 <- predict(ea1_fit_S2, Whole_S1, type="response") 
+  ea1_rf_S1 <- predict(ea1_fit_rf_S2, data=Whole_S1, type="response")$predictions
   ea0_S1 <- 1-ea1_S1
   ea0_rf_S1 <- 1-ea1_rf_S1
 
@@ -238,7 +275,16 @@ DR_split <- function(a, Trials){
 est <- Phi_hat(1, TrialData)-Phi_hat(0, TrialData)
 true <- Phi(1)-Phi(0)
 split_est <- DR_split(1, TrialData)-DR_split(0, TrialData)
-Res <- rbind(Res, c(true, est, split_est))
+Res <- c(true, est, split_est)
+return(Res)
 }
 
-write.csv(Res, "toyexample_res.R")
+results <- pbmcapply::pbmclapply(
+	1:100,
+	function(seed){
+		simulation(seed)
+	},
+	mc.cores = 10
+)
+saveRDS(results, "toyexample_n2000.rds")
+
